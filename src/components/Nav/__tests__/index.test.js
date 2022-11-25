561424d959c57eeddbb818b96fe42cc0061bfaa9
joseph <j.hernandez.92189@gmail.com>
2022-11-25 12:05:40 -0800

    added test

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -4,6 +4,23 @@ import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Nav from '..';
 
+const categories = [
+  { name: 'portraites', description: 'Portraits of people in my life' }
+]
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
+const mockContactSelected = jest.fn();
+const mockSetContactSelected = jest.fn();
+it('renders', () => {
+  render(<Nav
+    categories={categories}
+    setCurrentCategory={mockSetCurrentCategory}
+    currentCategory={mockCurrentCategory}
+    contactSelected={mockContactSelected}
+    setContactSelected={mockSetContactSelected}
+  />);
+})
+
 afterEach(cleanup);
 
 describe('Nav component', () => {
